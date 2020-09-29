913f503e26920f6b1f74eeb4f0786f411c1b937f
LuckyComputerKusmi <69892783+LuckyComputerKusmi@users.noreply.github.com>
2020-09-29 14:25:21 +0530

    Update application.js

--- application.js
+++ application.js
@@ -33,24 +33,7 @@ var submitRecommendation = function () {
   });
 };
 
-
-function stoppedTyping(){
-    if(this.value.length > 0) { 
-        document.getElementById('start_button').disabled = false; 
-    } else { 
-        document.getElementById('start_button').disabled = true;
-    }
-}
-function verify(){
-    if myText is empty{
-        alert "Put some text in there!"
-        return
-    }
-    else{
-      $(window).load(function () {
-        $("#recommendationForm").submit(submitRecommendation);
-           });
+$(window).load(function () {
+  $("#recommendationForm").submit(submitRecommendation);
+});
         
-    }
-}
-
